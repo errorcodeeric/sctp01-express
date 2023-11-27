3603ffaa552006d72ffa8ce17d991dc2c0a84421
kunxin-chor <chorkunxin@yahoo.com>
2023-11-27 13:48:28 +0000

    Added lucky number

--- 01-node-demo/index.js
+++ 01-node-demo/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();  // express function will return a new instance Express
+
+// setup some routes
+app.get("/", function(req,res){
+    res.send("<h1>hello world</h1>");
+})
+
+app.get("/about-us", function(req,res){
+    // `res` stands for response
+    res.send("About Us");
+})
+
+app.get("/lucky-number", function(req,res){
+    res.send("<h1>Your lucky number is " + parseInt(Math.random() * 1000 + 999)+"</h1>");
+})
+
+// starts the server
+app.listen(8000, function(){
+ console.log("Server has started")
+});
