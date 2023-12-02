fe1a30054306fe9850147830f490a9705d7d60f2
kunxin-chor <chorkunxin@yahoo.com>
2023-12-02 02:40:06 +0000

    Added a landing page with image and static files

--- 01-node-demo/readme.md
+++ 01-node-demo/readme.md
@@ -3,3 +3,6 @@
 2. `npm init` to create a new NodeJS application or `npm init -y` to skip all the questions
 3. Install Express with `npm install express`
 4. Create a new `.gitignore` file and add the following: `node_modules`
+5. For your Gitpod, run `npm install -g nodemon` everytime you restart Gitpod but
+if is using your own computer, then just once. The `-g` means `global` -- it will be 
+applied to all Node applications.
---
+++ 03-revision/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const wax = require('wax-on');
+const hbs = require('hbs');
+const app = express();
+
+wax.setLayoutPath('./views/layouts');
+wax.on(hbs.handlebars);
+
+app.use(express.static("public"));
+
+// set the default view eninge to 'hbs'
+app.set('view engine', 'hbs');
+
+app.get('/', function(req,res){
+    res.render('index');
+})
+
+app.listen(3000, function(req,res){
+    console.log("Server has start");
+})
