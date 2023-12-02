fe67038bf732079c803b17c8f8f41e3c3aee3c94
kunxin-chor <chorkunxin@yahoo.com>
2023-12-02 04:12:09 +0000

    Added form processing

---
+++ 05-form-processing/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const wax = require('wax-on');
+const hbs = require('hbs');
+const app = express();
+
+wax.setLayoutPath('./views/layouts');
+wax.on(hbs.handlebars);
+
+app.use(express.static("public"));
+
+// set the default view engine to 'hbs'
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({
+    extended: false // use basic forms not advanced forms
+}))
+
+// ROUTES BEGIN HERE
+app.get('/', function(req,res){
+    res.render('index');
+})
+
+// We want to add a rout for GET /add-food
+app.get("/add-food", function(req,res){
+    res.render('add-food');
+})
+
+// We want to add a route for POST /add-food
+// app.post("/add-food", function(req,res){
+//     const foodName = req.body.foodName;
+//     const calories = req.body.calories;
+//     res.render("food-summary", {
+//         foodName: foodName,
+//         calories: calories
+//     })
+// })
+
+app.post("/add-food", function(req,res){
+
+    // use the destructuring syntax to extract
+    // many keys from an object at one go
+    const {foodName, calories} = req.body;
+
+    res.render("food-summary", {
+        foodName,
+        calories
+    })
+})
+
+
+app.listen(3000, function(req,res){
+    console.log("Server has start");
+})
