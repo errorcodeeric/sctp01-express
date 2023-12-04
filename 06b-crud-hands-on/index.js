f21fcc34847d5ebe9dada23633513a5dd88cbe03
kunxin-chor <chorkunxin@yahoo.com>
2023-12-04 13:30:55 +0000

    Added edit example

--- 06b-crud-hands-on/index.js
+++ 06b-crud-hands-on/index.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({
     "extended": false
 }))
 
+
 // 1c setup wax-on
 wax.on(hbs.handlebars);
 
@@ -23,6 +24,11 @@ wax.setLayoutPath("./views/layouts");
 // 1e use static files (i.e, images, css, js etc. -- that is, all content not generated
 // by our routes)
 app.use(express.static('public'));
+
+// setup the 188 handlebars helpers
+const helpers = require('handlebars-helpers')({
+    handlebars: hbs.handlebars
+  });
 
 // Mock database
 let postings = [
@@ -64,7 +70,7 @@ app.get('/add-posting', function(req,res){
     res.render("add-posting");
 })
 
-
+// One route to process the submission of the create form
 app.post("/add-posting", function(req,res){
 
     let payments = req.body["payment-types"];
@@ -81,13 +87,102 @@ app.post("/add-posting", function(req,res){
         type: req.body.type
     }
 
-
-
     postings.push(newPosting);
     res.redirect("/");
 })
 
-// One route to process the submission of the create form
+// Route display the confirmation of delete
+app.get("/delete-posting/:postingId", function(req,res){
+    
+    // 1. Get the ID of the record from the URL parameters
+    const postingId = parseInt(req.params.postingId);
+    
+    // 2. find the record from the database
+    // let wantedPosting = null;
+    // for (let p of postings) {
+    //     // match by their id
+    //     if (p.id === postingId) {
+    //         wantedPosting = p;
+    //         break;
+    //     }
+    // }
+
+    let wantedPosting = postings.find(function(p){
+        return p.id === postingId;
+    });
+
+    // let wantedPosting = posting.filter(function(p){
+    //     return p.id===postingId;
+    // })[0];
+
+    res.render('delete-posting', {
+        "wantedPosting": wantedPosting
+    })
+})
+
+app.post("/delete-posting/:postingId", function(req,res){
+    const postingId = parseInt(req.params.postingId);
+
+    // use a full for loop to get the index
+    // let indexToDelete = null;
+    // for (let i = 0; i < postings.length; i++) {
+    //     if (postings[i].id === postingId) {
+    //         indexToDelete = i;
+    //         break;
+    //     }
+    // }
+
+    let indexToDelete = postings.findIndex(function(p){
+        return p.id === postingId;
+    })
+  
+    // let indexToDelete = -1;
+    // postings.forEach(function(p, index){
+    //     if (p.postingId === postingId) {
+    //         indexToDelete = index;
+    //     }
+    // }) 
+
+    if (indexToDelete != -1) {
+        // splice will modify the original array
+        postings.splice(indexToDelete, 1);
+    } else {
+        console.log("indexToDelete not found");
+    }
+
+    res.redirect("/");
+})
+
+app.get("/edit-posting/:postingId", function(req,res){
+    const postingToEdit = postings.find(p => p.id === parseInt(req.params.postingId));
+    console.log(postingToEdit);
+    res.render('edit-posting', {
+        postingToEdit
+    })
+})
+
+app.post("/edit-posting/:postingId", function(req,res){
+    // get the index of the posting that we want to change
+    const indexToEdit = postings.findIndex(p => p.id===parseInt(req.params.postingId));
+    if (indexToEdit != -1) {
+        // get the data from the form
+        let {title, price, type} = req.body;
+        let payments = req.body["payment-types"];
+        if (payments) {
+            payments = Array.isArray(payments) ? payments : [payments];
+        } else {
+            payments = [];
+        }
+        const modifiedProduct = {
+            id: parseInt(req.params.postingId), title, price, payments, type
+        }
+    
+        postings[indexToEdit] = modifiedProduct;
+        console.log(postings);
+    }
+
+    res.redirect("/");
+})
 
 // 3. START SERVER (No routers after you've started server)
 app.listen(3000, function(){
